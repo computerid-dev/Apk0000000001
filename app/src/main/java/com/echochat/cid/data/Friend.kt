d2816b0cd2356d6270037fcf69de786a04c6b114
computerid-dev <nugrohokelyn@gmail.com>
2026-08-04 15:57:32 +0700

    Upload app/src/main/java/com/echochat/cid/data/Friend.kt

---
+++ app/src/main/java/com/echochat/cid/data/Friend.kt
@@ -0,0 +1,17 @@
+package com.echochat.cid.data
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "friends",
+    indices = [Index(value = ["friendUid"], unique = true)]
+)
+data class Friend(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val friendUid: String,
+    val nickname: String,
+    val addedAt: Long = System.currentTimeMillis()
+)
